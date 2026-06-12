2ea6959b2cd7adaa9e008a4c7f32e175bda24420
Edbornav <duadobn@gmail.com>
2026-06-11 17:19:26 -0700

    Config for Vercel deployment

--- ENTERTAINMENT_REVIEWS_FRONT/Assets/js/api.js
+++ ENTERTAINMENT_REVIEWS_FRONT/Assets/js/api.js
@@ -1,4 +1,6 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : 'https://TU-BACKEND-EN-RENDER.com/api'; // Cambiar por la URL real del backend
 const CLOUDINARY_CLOUD_NAME = 'dfcbsqxfq';
 const CLOUDINARY_UPLOAD_PRESET = 'IMAGENES';
 
